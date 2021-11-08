5fb164b3f8197e06adc64d38dd43897ede002336
Bence A. Tóth <tothab@gmail.com>
2021-11-08 18:32:25 +0100

    Clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        placeholder="Search"
+        type="search"
+        value={query}
+        onChange={(event) => {
+          setQuery(event.target.value);
+        }}
+      />
     </div>
   );
 }
